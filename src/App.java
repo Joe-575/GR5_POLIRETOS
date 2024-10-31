c7f0807d0098a921e6eaf6e68987367a0070b24f
Joe-575 <medinajoel814@gmail.com>
2024-10-31 22:48:19 +0000

    cambio en la clase principal

--- src/App.java
+++ src/App.java
@@ -2,8 +2,7 @@ import seriesnumericas.Gr5Serie;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Gr5Serie serie =  new Gr5Serie();
-        serie.Gr5_crearSerieFibonacci();
-        serie.Gr5_SerieImpares();
+        Gr5Serie.Gr5_crearSerieFibonacci();
+        Gr5Serie.Gr5_SerieImpares();
     }
 }
